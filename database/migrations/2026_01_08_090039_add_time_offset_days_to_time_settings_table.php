9a1a75bb4a8bf87f31c8145d5e5cf224b7c9fec6
Agus Prasetya <agusprasetyaaaa@gmail.com>
2026-01-27 15:31:56 +0700

    Fix migration time configuration

--- database/migrations/2026_01_08_090039_add_time_offset_days_to_time_settings_table.php
+++ database/migrations/2026_01_08_090039_add_time_offset_days_to_time_settings_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('time_settings', function (Blueprint $table) {
-            //
+            $table->integer('time_offset_days')->default(0)->after('testing_mode');
         });
     }
 
@@ -22,7 +21,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('time_settings', function (Blueprint $table) {
-            //
+            $table->dropColumn('time_offset_days');
         });
     }
 };
