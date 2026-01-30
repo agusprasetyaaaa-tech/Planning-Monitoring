4bcff2ac64ed6d0f90cebd53028135796834b93d
Agus Prasetya <agusprasetyaaaa@gmail.com>
2026-01-30 13:25:15 +0700

    Refactor middleware setup in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,16 +10,19 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
+        // 1. Tambahkan TrustProxies agar Laravel mengenali HTTPS dari Nginx
+        $middleware->trustProxies(at: '*');
+
+        // 2. Middleware SecurityCheck yang Anda miliki
         $middleware->append(\App\Http\Middleware\SecurityCheck::class);
 
+        // 3. Middleware untuk Inertia dan Assets
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
